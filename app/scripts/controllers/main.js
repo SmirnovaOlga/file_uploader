fcfb3f2989936730506b41c79ed4f532ee7b91c2
SmirnovaOlga <OlgaVSmir@gmail.com>
2013-10-28 17:11:50 +0400

    All files.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fileUploaderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
