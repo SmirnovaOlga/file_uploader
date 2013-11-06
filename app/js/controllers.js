0e0b55b6918fe1e1331a74416307843d8a84f54c
SmirnovaOlga <OlgaVSmir@gmail.com>
2013-11-06 13:32:23 +0400

    Refactoring.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,65 +3,60 @@
 /* Controllers */
 
 angular.module('fileUploaderApp.controllers', []).
-  controller('IndexCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+    controller('IndexCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
 
-		$scope.path = '/';
+        $scope.path = '/';
 
-		if($routeParams.path) {
+        if ($routeParams.path) {
 
-			$scope.path = $routeParams.path;
-		}
+            $scope.path = $routeParams.path;
+        }
 
-		prepareBreadcrumbs();
+        prepareBreadcrumbs();
 
-		function prepareBreadcrumbs() {
+        function prepareBreadcrumbs() {
 
-			$scope.breadcrumbs = [];
+            $scope.breadcrumbs = [];
 
-			var splitPath = $scope.path.split('/');
-			var link = '';
+            var splitPath = $scope.path.split('/');
+            var link = '';
 
-			for(var i = 0; i < splitPath.length; i++) {
+            for (var i = 0; i < splitPath.length; i++) {
 
-				link += '/' + splitPath[i];
+                link += '/' + splitPath[i];
 
-				var obj = {
-					title: splitPath[i],
-					link: link
-				};
+                var obj = {
+                    title: splitPath[i],
+                    link: link
+                };
 
-				$scope.breadcrumbs.push(obj);
-			}
-		}
+                $scope.breadcrumbs.push(obj);
+            }
+        }
 
         $scope.response = {
-        "name": ["folder1", "folder2", "folder3"]
+            "name": ["folder1", "folder2", "folder3"]
         }
 
         prepareTree($scope.response);
 
         function prepareTree(response) {
-            var response = response;
             $scope.trees = [];
             var names = response.name;
             var link = '';
 
 
-            for(var i = 0; i < names.length; i++) {
+            for (var i = 0; i < names.length; i++) {
                 link += '/' + names[i];
 
                 var obj = {
                     title: names[i],
                     link: link
                 };
-                var array = $scope.trees.push(obj);
+                $scope.trees.push(obj);
             }
-
-            $scope.trees.push(obj);
 
         }
 
 
-
-
-  }]);
+    }]);
