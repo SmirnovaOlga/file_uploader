e5fa34da2066154475eaf4f24bd57148e63be4a2
SmirnovaOlga <OlgaVSmir@gmail.com>
2013-11-08 16:54:38 +0400

    New changes.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -48,38 +48,64 @@ angular.module('fileUploaderApp.controllers', []).
                             {
                                 type: "folder",
                                 name: "images",
-                                path: "/animals/cat/images"
+                                path: "/animals/cat/images",
+                                children: [
+                                    {
+                                        type: "file",
+                                        name: "cat001.jpg",
+                                        path: "/animals/cat/images/cat001.jpg"
+                                    },
+                                    {
+                                        type: "file",
+                                        name: "cat002.jpg",
+                                        path: "/animals/cat/images/cat002.jpg"
+                                    }
+                                ]
                             },
                             {
                                 type: "folder",
                                 name: "images2",
-                                path: "/animals/cat/images2"
+                                path: "/animals/cat/images2",
+                                children: [
+                                    {
+                                        type: "file",
+                                        name: "cat003.jpg",
+                                        path: "/animals/cat/images2/cat003.jpg"
+                                    },
+                                    {
+                                        type: "file",
+                                        name: "cat004.jpg",
+                                        path: "/animals/cat/images2/cat004.jpg"
+                                    }
+                                ]
                             }
                         ]
                     }
                 ]
             },
-			{
-				type: "folder",
-				name: "animals2",
-				path: "/animals2",
-				children: [
-					{
-						type: "folder",
-						name: "cat2",
-						path: "/animals2/cat2"
-					}
-				]
-			}
+            {
+                type: "folder",
+                name: "animals2",
+                path: "/animals2",
+                children: [
+                    {
+                        type: "folder",
+                        name: "cat2",
+                        path: "/animals2/cat2"
+                    }
+                ]
+            }
         ];
 
-		$scope.tree = prepareTree($scope.response);
+        $scope.tree = prepareTree($scope.response);
 
         function prepareTree(tree) {
 
             var array = [];
+            $scope.files = [];
             var element = {};
             var len = tree.length;
+            var parent = {};
 
             for (var i = 0; i < len; i++) {
                 var level = 0;
@@ -88,42 +114,36 @@ angular.module('fileUploaderApp.controllers', []).
             }
 
             function traversalTree(el) {
-
                 if (el.children) {
 
                     array.push({name: el.name, path: el.path, type: el.type, level: level});
 
-                        level++;
+                    level++;
 
-
-					for (var j = 0; j < el.children.length; j++) {
-                        if(j > 0)
-                        {
-                            level--;
+                    for (var j = 0; j < el.children.length; j++) {
+                        if (el.children[j].type != "file")
+                            traversalTree(el.children[j]);
+                        else {
+                            $scope.files.push({name: el.children[j].name, path: el.children[j].path, type: el.children[j].type, parent_path: el.path});
                         }
-                        traversalTree(el.children[j]);
-
-					}
+                    }
 
                 }
                 else {
 
-					if(el.length) {
+                    if(el.length) {
 
-						for (var k = 0; k < el.length; k++) {
-							array.push({name: el[k].name, path: el[k].path, type: el.type, level: level});
-						}
-					}
-					else {
-
-						array.push({name: el.name, path: el.path, type: el.type, level: level});
-
-					}
+                        for (var k = 0; k < el.length; k++) {
+                            array.push({name: el[k].name, path: el[k].path, type: el.type, level: level});
+                        }
+                    }
+                    else {
+                        array.push({name: el.name, path: el.path, type: el.type, level: level});
+                    }
                 }
             }
-            console.log(array);
 
-			return array;
+            return array;
 
         }
 
